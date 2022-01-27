310e3f1fb44c610ca3e9bdf00fbdb3e2ffdcf67a
11Kclarke <hr19845@bristol.ac.uk>
2022-01-27 13:17:10 +0000

    Clean +tactical
    added comments removed legacy code, organized code into functions, implemented simple tactical voting option.

--- voronoi.py
+++ voronoi.py
@@ -14,10 +14,14 @@ import sympy as sp
 [ymin, ymax] = -2.1, 2.1
 x = sp.symbols('x')
 y = sp.symbols('y')
+popfieldedge = 10
+tacticalportion = 1/3
 
-points = np.array([[0, 0], [-1, -1],
-                   [0.5,0.5],[-0.5,0.5]])
+points = np.array([[-0.9,-0.9],[0,0],
+                   [0.5,0.5],[-0.5,0.5],[-1.1,-1.1],[-1,-1],[-1.1,-1],[-1,-1.1]])
+
 centroids = points
+numberofregions = len(centroids)
 points = np.append(points, [[1000, 1000], [-1000, 1000], [1000, -1000], [-1000, -1000]], axis=0)
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
@@ -36,6 +40,7 @@ plt.xlim([xmin, xmax]), plt.ylim([ymin, ymax])
 
 colors = cm.rainbow(np.linspace(1, 0, len(points) - 4))
 labels = []
+
 centroids = []
 for point in range(len(points[0:len(points) - 4])):
     centroids.append(points[point])
@@ -43,6 +48,7 @@ for point in range(len(points[0:len(points) - 4])):
     plt.scatter(points[point][0],points[point][1],zorder =2,color=colors[point])
 
     labels.append(temp)
+
 ax.legend(labels, fontsize="x-small")
 
 coords = [[-2, 2], [2, 2], [2, -2], [-2, -2]]
@@ -100,7 +106,7 @@ for region in realregions:
             vertexofregion.append(sp.Point(vor.vertices[region[point - 1]]))
     vertexcoords.append(vertexofregion)  # all coordinates of vertexes sorted by region
 colors = cm.rainbow(np.linspace(1, 0, len(vertexcoords)))
-newvertexcoords = []
+
 
 yaxis = sp.Line(sp.Point(10, 10), sp.Point(-10, -10))
 xaxis = sp.Line(sp.Point(10, 0), sp.Point(-10, 0))
@@ -132,96 +138,61 @@ def comparepoint(p1, p2, centroid):  # copied from stackoverflow
     return d1 > d2;
 
 
+
+"""uses comparepoints to order points in each region, by angle, allows drawing of shapes
+just bubble sort but compare func instead of < or >"""
 k = -1
 for region in vertexcoords:
     k += 1
 
     centroid = sp.Point([sum(x) / len(x) for x in zip(*region)])
     # print(region)
-    for p in region:
-        print(sp.Line(centroid, p).angle_between(xaxis).evalf())
-    for i in range(len(region)):
-        # We want the last pair of adjacent elements to be (n-2, n-1)
+    #finds new centroid as average of each vertex of region
+    #for p in region:
+    #print(sp.Line(centroid, p).angle_between(xaxis).evalf())
+    for i in range(len(region)):        
         for j in range(len(region) - 1):
             if comparepoint(region[j], region[j + 1], centroid):
                 # Swap
                 region[j], region[j + 1] = region[j + 1], region[j]
-                print(region[i].evalf())
-                print("is greater than")
-                print(region[i + 1].evalf())
-
-    # region.sort(key=lambda x: sp.Line(x,centroid).angle_between(xaxis).evalf(), reverse=True)
-    for p in region:
-        print(sp.Line(centroid, p).angle_between(xaxis).evalf())
-
-    print("-----------------------")
+                
 
     vertexcoords[k] = region
-    # print(region)
-    # print(i)
 
-# print(np.shape(newvertexcoords))
-# print(newvertexcoords)
-for i in newvertexcoords:
-    print(i)
-    for j in i:
-        print(j)
-        vertexcoords[i, j] = sp.Point(j)
-n = 5
-centrepopulation = 10
-
-i = -1
-
-layers = 4 # increase for better volume accuracy
-values = np.linspace(2, -2, n)
 def popfunc(x, y, centrepopulation=8):
     return centrepopulation - x * 2 - y * 2
-def popfield(centrepopulation=8,N=n):
+
+def popfield(centrepopulation=8,N=popfieldedge):
     X = np.linspace(-2, 2, N)
     Y = np.linspace(-2, 2, N)
     population = [[0 for x in range(N)] for y in range(N)]
     i=-1
-    j=-1
-    #X, Y = np.meshgrid(x, y)   
-    
+    j=-1   
     for x in X:
         #print(x)
         i+=1
         for y in Y:
-            j+=1
-            print((x,y))
-            print("i:"+str(i))
-            print("j:"+str(j))
-            population[i][j]=popfunc(x,y)#population density function
-            print(population[i][j])
+            j+=1            
+            population[i][j]=popfunc(x,y)#population density function           
         j=-1
         population = np.asarray(population)
     return population
 
 
-pop = popfield()
-def tactical(pop):
-    print(pop)
-    values = np.linspace(2, -2, n)
-    
-    tacticalpop = pop*1/3
+
+def tactical(pop,portion = tacticalportion):
+    pop=np.array(pop)
+    tacticalpop = pop*portion
     pop = pop -tacticalpop
-    print(tacticalpop)
-    print("--------------------------------------------------\n\n\n\n--------------------------------------------------------------")
+    #print(tacticalpop)
+    #print("--------------------------------------------------\n\n\n\n--------------------------------------------------------------")
     return pop, tacticalpop
 
-print(np.size(pop))
-print(pop)
-(pop,tacticalpop) = tactical(pop)
-print(pop)
-print(tacticalpop)
-sregions=[]
-regionvolumes=[]
-for region in vertexcoords:
-    sregions.append(sp.Polygon(*region))
-    regionvolumes.append(0)
+
+
 
 """
+#will at some point be a recusive nd integration 
 def integrate(region,n,pop,sregions,regionpops,dim,depth,current=np.zeros(dim)):#dim long list of 0s
     c=-1
     
@@ -241,20 +212,143 @@ def integrate(region,n,pop,sregions,regionpops,dim,depth,current=np.zeros(dim)):
                 
                 return depth - 1, 
 """
-xcount=-1
-for x in values:
-    xcount+=1
-    ycount =-1
-    for y in values:
-        ycount+=1
-        i=-1
-        
-        for region in sregions:
-            i+=1
-            if region.encloses_point(sp.Point(x,y)):
-                regionvolumes[i]+=pop[xcount,ycount]
+
+def volume2d(pop,sregions):
+    """this is 99% of run time and disgustingly inneficient if someone could make it faster would be great"""
+    #pop2d array of populations sregions regions as sympy 2dshapes
+    regionvolumes= np.zeros(numberofregions)
+    xcount=-1
+    n=len(pop)
+    values = np.linspace(-2,2,n)
+    for x in values:
+        xcount+=1
+        ycount =-1
+        for y in values:
+            ycount+=1
+            i=-1
+            #print("checking:")
+            #print(str((xcount,ycount)))
+            #print("@")
+            #print(str((x,y)))
+            for region in sregions:
+                i+=1
+                if region.encloses_point(sp.Point(x,y)):
+                    print(str((x,y)))
+                    print("in")
+                    regionvolumes[i]+=pop[xcount,ycount]
+    regionvolumes = np.array(regionvolumes)
+    print(regionvolumes)
+    return regionvolumes            
+
+def tactvoting(tacticalpop,regionvolumes,centroids):
+    i=-1
+    #tactical pop is 1/3 of total pop array
+    n=np.shape(tacticalpop)[0]    
+    values = np.linspace(-1.999,1.999,n)
+    tacchoices = np.zeros(np.shape(tacticalpop))
+    
+    """for each row take each square and compare with each region"""
+    for x in tacticalpop:
+        #x is row of tactical population
+        i+=1
+        j=-1
+        for y in x:
+            #y is individual square on view space of those tactically voting
+            j+=1
+            maxratio=0
+            coords = (values[i],values[j])
+            
+            regionindex =0
+            actualbelief = np.pi#placeholder
+            for region in sregions:
+                regionindex +=1
+                if region.encloses_point(coords):                
+                    actualbelief = regionindex
+                    regionchoice = regionindex
+                    #print(coords)
+            if actualbelief == np.pi: 
+                print(coords)
+                print("somehow not in any region")
+                regionchoice = 1/0
+            for k in range(len(regionvolumes)):
+            #bigvolume high chance to win low distance better views                 
+                   a = regionvolumes[k]/(((centroids[k][0]- coords[0])**2 +(centroids[k][1]- coords[1])**2)**(1/2))                  
+                   if a>maxratio:
+                       maxratio=a
+                       regionchoice =(k)
+            #print(tacchoices)
+            #if regionchoice != actualbelief and regionchoice !=-1:
+                #print("\n\n\n point at: "+str((coordx,coordy))+" inside region: "+str(regionindex))
+                #print("will tactically vote for: "+str(regionchoice)+" ,but is closer in belief to: "+str(actualbelief))
                 
-"""           
+            tacchoices[i][j]= regionchoice
+    return tacchoices
+
+
+#sets ordered lists of points to sympy 2d shapes
+sregions=[]
+regionvolumes=[]
+
+for region in vertexcoords:
+    sregions.append(sp.Polygon(*region))
+    regionvolumes.append(0)
+    
+    
+pop = popfield()#2d array of population at each view
+
+
+print("finding volumes no tactical")                
+regionvolumes = volume2d(pop,sregions)
+print("found")
+(popnotac,tacticalpop) = tactical(pop)
+portionofvolumes = 2/3*np.array(regionvolumes)
+tactchoices = tactvoting(tacticalpop,regionvolumes,centroids)
+
+
+tacticalonly = np.zeros(np.shape(portionofvolumes))
+k=-1
+
+tactchoices
+for row in tactchoices:
+    k+=1
+    j=0
+    print(row)
+    for choice in row:                   
+        tacticalonly[int(choice)-1] += tacticalpop[k][j]#idk why all choices 1 to high
+        j+=1
+        
+      
+voteswithtactical = np.add(tacticalonly,portionofvolumes)        
+
+n = -1
+
+
+for region in vertexcoords:
+    n += 1
+    shape = [i.coordinates for i in region]
+    plt.fill(*zip(*shape))
+    print("--------")
+    print("region:" + str(n))
+    print("area:" + str(abs(sp.Polygon(*region).area.evalf())))
+    
+    print("volume/votes of region:" + str(regionvolumes[n]))
+    print("Including Tactical voting:"+str(voteswithtactical[n]))
+    print("region centroid"+str(centroids[n]))
+    #for point in region:
+        #print(point.evalf())
+        #plt.scatter(point.coordinates[0], point.coordinates[1], color=colors[n], zorder=2)
+    # plt.imshow(population, cmap='hot', interpolation='nearest')
+
+
+#for i in range(len(points)):
+    #lines.append(sp.Line(points[i - 1], points[i]))
+    # print(lines[i].plot_interval())
+    # print(lines[i].equation())
+
+    #plt.plot([coords[i - 1][0], coords[i][0]], [coords[i - 1][1], coords[i][1]], color="black", zorder=3)
+
+plt.show()
+"""          
 j = -1
 sregion = []
 i = -1
@@ -289,85 +383,5 @@ for region in vertexcoords:
     regionvolumes.append(volumeofregion)
 n = -1
 #print(regionvolumes)
+
 """
-totalarea = 0
-
-
-
-def tactvoting(tacticalpop,regionvolumes,centroids):
-    i=-1
-    
-    n=np.shape(tacticalpop)[0]
-    print(n)
-    values = np.linspace(-2,2,n)
-    tacchoices = [[0 for x in range(n)] for y in range(n)]
-    print()
-    print("TACT")
-    #print(tacticalpop)
-    for x in tacticalpop:
-        print("x is:")
-        print(x)
-        i+=1
-        j=-1
-        for y in x:
-            j+=1
-            coordx = values[i]
-            coordy = values[j]
-            maxratio=0
-            coords = (coordx,coordy)
-            regionchoice = -1
-            for k in range(len(regionvolumes)):
-                   #print(centroids)
-                   a = regionvolumes[k]/((centroids[k][0]- coordx)**2 +(centroids[k][1]- coordy)**2)**1/2
-                   if a>maxratio:
-                       maxratio=a
-                       regionchoice =k
-            #print(tacchoices)
-            tacchoices[i][j]= regionchoice
-    return tacchoices
-tactchoices = tactvoting(tacticalpop,regionvolumes,centroids)
-print(tactchoices)
-k=-1
-withtac = regionvolumes
-print(tacticalpop)
-print(withtac)
-
-for row in tactchoices:
-    print(row)
-    k+=1
-    j=-1
-    for choice in row:
-        j+=1
-        
-        print(withtac[choice])
-        withtac[choice]+=tacticalpop[k,j]
-        
-        print("after")
-        print(withtac[choice])
-    
-n = -1
-
-for region in vertexcoords:
-    n += 1
-    shape = [i.coordinates for i in region]
-    plt.fill(*zip(*shape))
-    print("--------")
-    print("region:" + str(n))
-    print("area:" + str(abs(sp.Polygon(*region).area.evalf())))
-    totalarea += abs(sp.Polygon(*region).area.evalf())
-    print("volume/votes of region:" + str(regionvolumes[n]))
-    print("Including Tactical voting:"+str(withtac[n]))
-    #for point in region:
-        #print(point.evalf())
-        #plt.scatter(point.coordinates[0], point.coordinates[1], color=colors[n], zorder=2)
-    # plt.imshow(population, cmap='hot', interpolation='nearest')
-print(totalarea)
-
-for i in range(len(points)):
-    lines.append(sp.Line(points[i - 1], points[i]))
-    # print(lines[i].plot_interval())
-    # print(lines[i].equation())
-
-    #plt.plot([coords[i - 1][0], coords[i][0]], [coords[i - 1][1], coords[i][1]], color="black", zorder=3)
-
-plt.show()
