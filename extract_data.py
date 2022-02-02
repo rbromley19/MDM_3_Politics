132ca6854871d7a1b732537432bc4952428282cd
cfrsr <60779426+cfrsr@users.noreply.github.com>
2022-02-02 13:17:34 +0000

    added NaN removal to extract_data

--- extract_data.py
+++ extract_data.py
@@ -22,10 +22,24 @@ def extract_data(input_name,output_name,output_or_return):
     total_list = [economy_list,enviornment_list,immig_list,health_list,brexit_list]
     flat_list = [item for l in total_list for item in l]
     flat_list.insert(0,'partyId')
-    print(flat_list)
+  
     
     extracted_data = df.loc[:,flat_list]
     
+    #cleaning the data, removing the null values and replacing with 'Don't know', this was checked properly and so there are no additional values
+    extracted_data['remainVnodeal'] = extracted_data['remainVnodeal'].replace(9999.0, "Don't know") 
+    extracted_data['remainVnodeal'] = extracted_data['remainVnodeal'].fillna("Don't know")
+    extracted_data['dealVremain'] = extracted_data['dealVremain'].fillna("Don't know")
+    extracted_data['euRefVote'] = extracted_data['euRefVote'].fillna("Don't know")
+    extracted_data['selfPriorities_nhs'] = extracted_data['selfPriorities_nhs'].fillna("Don't know")
+    extracted_data['selfPriorities_brexit']=extracted_data['selfPriorities_brexit'].fillna("Don't know")
+    extracted_data['deficitReduce'] = extracted_data['deficitReduce'].fillna("Don't know")
+    extracted_data['selfPriorities_econ'] = extracted_data['selfPriorities_econ'].fillna("Don't know")
+    extracted_data['climateChange'] =  extracted_data['climateChange'].fillna("Don't know")
+    extracted_data['selfPriorities_environment'] = extracted_data['selfPriorities_environment'].fillna("Don't know")
+    
+    
+    #0 for file output 1 for return
     if output_or_return ==0:
         extracted_data.to_csv(output_file)
     elif output_or_return == 1:
